66c2f7bfbbc820e188faa4267972a572b36ad126
Justin Roger <justin@Justins-MacBook-Pro.local>
2020-08-19 09:14:02 -0400

    fintness

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 8080;
+
+const db = require("./models");
+const app = express();
+const databaseName = "workout_db"
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "", { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log(`Successfully connected to database: ${databaseName}`))
+
+app.use("/api", require("./routes/api-routes.js"));
+app.use("/", require("./routes/html-routes.js"));
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
